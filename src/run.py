26a7156264a03f90410e6bbe8af4c70b3dbcc60f
grapix <anthony.kalayjdjian@outlook.com>
2024-08-27 14:29:28 +0000

    prettified

--- src/run.py
+++ src/run.py
@@ -3,11 +3,11 @@ import sys
 from my_ingestion import run_model
 import time
 
-src_dir = os.path.dirname(os.path.abspath(__file__))        
-BENCHMARK_PATH = os.path.join(src_dir, "..", "benchmark.sav")             # Add the path to the saved benchmark file here. If the file does not exist, it will be created there.
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))        
+BENCHMARK_CACHE_DIR = os.path.join(SRC_DIR, "..", "Benchmarks")             # Path to the benchmark folder. If the file does not exist, it will be created there.
 
 def run(model_name):
-    model_path = os.path.join(src_dir, "models", model_name)
+    model_path = os.path.join(SRC_DIR, "models", model_name)
 
     print("\n=====================================================")
     print("Starting the model training and evaluation process...")
@@ -16,7 +16,7 @@ def run(model_name):
     start = time.time()
 
     # training & evaluating the model
-    run_model(src_dir, model_path, BENCHMARK_PATH)
+    run_model(SRC_DIR, model_path, regime="scarce", benchmark_dir=BENCHMARK_CACHE_DIR)
 
     end = time.time()
     print("\n===============================================")
@@ -26,18 +26,10 @@ def run(model_name):
 
 if __name__ == "__main__":
     # read input in command line
-    model_name_list = sys.argv[1:]
-    if len(model_name_list)==0:
-        raise ValueError("Provide at least one model name to run the evaluation on e.g. <bi_transformer>")
+    model_name = sys.argv[1]
     
-    print(f"Using models: {model_name_list}")
-    for model_name in model_name_list:
-        if not os.path.exists(os.path.join(src_dir, "models", model_name)):
-            raise ValueError(f"Model name {model_name} does not exist, provide a valid model name e.g. <bi_transformer>")
-    
-    for model_name in model_name_list:
-        try:
-            run(model_name)
-        except:
-            print(f"Error in {model_name}, continuing the evaluation with the next model...")
-            continue
+    print(f"Using model: {model_name}")
+    if not os.path.exists(os.path.join(SRC_DIR, "models", model_name)):
+        raise ValueError(f"Model name {model_name} does not exist, provide a valid model name e.g. <bi_transformer>")
+
+    run(model_name)
